0f014d3768971b1a75f64435db4089a47131d996
Robert Hutchins <rjhutchins22@gmail.com>
2017-07-20 15:59:28 -0400

    The app is working now

--- star-wars-react/src/components/Jumbotron.js
+++ star-wars-react/src/components/Jumbotron.js
@@ -1,48 +1,13 @@
 import React, {Component} from 'react';
 
 export default class Jumbotron extends Component {
-  constructor() {
-    super();
-    this.state = {
-      vehicles: []
-    }
-  }
-
-  componentDidMount {
-    fetch('https://swapi.co/api/vehicles/')
-    .then(response => response.json)
-    .then(response => {
-      let vehicles = response.results.map((vehicle) => {
-        return(
-          <div class="card" style="width: 20rem;">
-            <div class="card-block">
-              <h4 class="card-title">Vehicle: {vehicle.name}</h4>
-              <h6 class="card-subtitle mb-2 text-muted">Model: {vehicle.model}</h6>
-              <ul class="list-group list-group-flush">Specs
-                <li class="list-group-item">Manufacturer: {vehicle.manufacturer}</li>
-                <li class="list-group-item">Class: {vehicle.vehicle_class}</li>
-                <li class="list-group-item">Passengers: {vehicle.passengers}</li>
-                <li class="list-group-item">Crew: {vehicle.crew}</li>
-
-                <li class="list-group-item">Length: {vehicle.length}</li>
-                <li class="list-group-item">Max Speed: {vehicle.max_atomosphering_speed}</li>
-                <li class="list-group-item">Cargo Capacity: {vehicle.cargo_capacity}</li>
-
-              </ul>
-            </div>
-          </div>
-        )
-      })
-    } )
-  }
-
 
   render(){
     return(
       <div className="jumbotron">
-        <h1 className="header">Star Wars</h1>
-        <hr />
-        <span>The Vehicles of Star Wars</span>
+        <h1 className='header'>Star Wars</h1>
+        <hr></hr>
+        <h3>The Vehicles of Star Wars</h3>
       </div>
     )
   }
--- star-wars-react/src/components/Vehicle_Cards.js
+++ star-wars-react/src/components/Vehicle_Cards.js
@@ -1,10 +1,47 @@
 import React, {Component} from 'react';
 
 export default class Cards extends Component {
+  constructor() {
+    super();
+    this.state = {
+      vehicles: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://swapi.co/api/vehicles/')
+    .then(resp => resp.json())
+    .then(resp => {
+      let swVehicles = resp.results.map((vehicle) => {
+        return(
+          <div className="card" key={vehicle.id} style={{width: 20+ 'em'}}>
+            <div className="card-block">
+              <h4 className="card-title">Vehicle: {vehicle.name}</h4>
+              <h6 className="card-subtitle mb-2 text-muted">Model: {vehicle.model}</h6>
+              <ul className="list-group list-group-flush">Specs
+                <li className="list-group-item">Manufacturer: {vehicle.manufacturer}</li>
+                <li className="list-group-item">Class: {vehicle.vehicle_class}</li>
+                <li className="list-group-item">Passengers: {vehicle.passengers}</li>
+                <li className="list-group-item">Crew: {vehicle.crew}</li>
+
+                <li className="list-group-item">Length: {vehicle.length}</li>
+                <li className="list-group-item">Max Speed: {vehicle.max_atomosphering_speed}</li>
+                <li className="list-group-item">Cargo Capacity: {vehicle.cargo_capacity}</li>
+              </ul>
+            </div>
+          </div>
+        )
+      });
+      this.setState({vehicles: swVehicles});
+    })
+  }
+
+
+
   render(){
     return(
       <div className="vehicles">
-        <span>This is going to be a Vehicle_Cards</span>
+        {this.state.vehicles}
       </div>
     )
   }
